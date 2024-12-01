271ae584277e71cd5e20ad9f729269a58f990bfb
David Park <dpark15@login-i16.local>
2024-12-01 03:08:45 -0800

    sumArraysOnHost_v1

--- vector_addition/sumArraysOnHost.c
+++ vector_addition/sumArraysOnHost.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
@@ -17,8 +18,22 @@ void initialData(float *ip, int size) {
     }
 }
 
+void saveArrayToFile(const char *filename, float *array, int size) {
+	FILE *file = fopen(filename, "a");
+	if (file == NULL) {
+		printf("Error opening file %s for writing.\n", filename);
+		exit(1);
+	}
+	fprintf(file, "\n === ARRAY === \n");
+	for (int i = 0; i < size; i++) {
+		fprintf(file, "%f\n", array[i]); // Write each element to a new line
+	}
+	fprintf(file, "\n");
+	fclose(file);
+}
+
 int main (int argc, char **argv) {
-    int nElem = 1024;
+    int nElem = 10;
     size_t nBytes = nElem * sizeof(float);
 
     float *h_A, *h_B, *h_C;
@@ -30,6 +45,10 @@ int main (int argc, char **argv) {
     initialData(h_B, nElem);
 
     sumArraysOnHost(h_A, h_B, h_C, nElem);
+
+    saveArrayToFile("h_A_B_C.txt", h_A, nElem);
+    saveArrayToFile("h_A_B_C.txt", h_B, nElem);
+    saveArrayToFile("h_A_B_C.txt", h_C, nElem);
 
     free(h_A);
     free(h_B);
